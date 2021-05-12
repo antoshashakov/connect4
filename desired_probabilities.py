ba977e3b21f1e200e0013ff37d6143a4ed64258d
ipriddle <84038467+ipriddle@users.noreply.github.com>
2021-05-12 11:12:48 -0400

    Added earliest version of desired probabilities

---
+++ desired_probabilities.py
@@ -0,0 +1,81 @@
+import math
+import numpy as np
+import random
+
+
+# VERY EARLY version of the desired probabilities code; there are likely many ways in which this can be optimized,
+# and I haven't yet had the chance to test it
+
+def soft_max(arr1):
+    # total to later divide each value by
+    total = 0
+    # list of probabilities to later be made into an array
+    probs = []
+    # add exponential values to the probabilities, and determine the total
+    for i in arr1:
+        probs.append(math.exp(i))
+        total += math.exp(i)
+    # divide each value by the total
+    for j in range(len(probs)):
+        probs[j] = probs[j] / total
+    return np.array(probs)
+
+
+# we need to be given the two boards and the model
+def desired_probabilities(board1, board2, model):
+    # number of test games we will play
+    trials = 100
+    # the list storing the win/loss/draw stats
+    stats = [0, 0, 0]
+    # play the given number of games
+    for i in range(trials):
+        # play until the game is finished
+        while True:
+            # get the probability distribution from the model
+            prob_distribution = soft_max(model.predict(board1, board2))
+            # pick a column at random using the helper function
+            column = pick_probability(prob_distribution)
+            # make a move
+            board1, board2, result = make_move(board1, board2, column)
+            # check if the game is over
+            if result == 1:
+                stats[0] += 1
+                break
+            if result == -1:
+                stats[1] += 1
+                break
+            if result == 2:  # TODO: currently assuming that a draw is represented by a 2
+                stats[2] += 1
+                break
+            # if the game is still going, we now play as the other player (we swap the order of several things)
+            # get the probability distribution from the model
+            prob_distribution = soft_max(model.predict(board2, board1))
+            # pick a column at random using the helper function
+            column = pick_probability(prob_distribution)
+            # make a move
+            board2, board1, result = make_move(board2, board1, column)
+            # check if the game is over
+            if result == 1:
+                stats[1] += 1
+                break
+            if result == -1:
+                stats[0] += 1
+                break
+            if result == 2:  # TODO: currently assuming that a draw is represented by a 2
+                stats[2] += 1
+                break
+    stats_arr = np.array(stats)
+    return soft_max(stats_arr)
+
+
+# assumes that arr1 represents percentage chances
+def pick_probability(arr1):
+    # generate a random number
+    r = random.random()
+    # total to keep track of the range we will check
+    total = 0
+    for i in range(len(arr1)):
+        total += arr1[i]
+        if r <= total:
+            return i + 1
+    return len(arr1) + 1
