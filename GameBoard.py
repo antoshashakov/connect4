1b65b99e3e96e1ee2dbe96e0a174ff9932b42d3d
ThinhTTruong <84049601+ThinhTTruong@users.noreply.github.com>
2021-05-13 15:18:34 -0400

    Add files via upload

--- GameBoard.py
+++ GameBoard.py
@@ -78,13 +78,92 @@ class GameBoard:
     def getBoards(self):
         return self.gameBoards[0], self.gameBoards[1]
 
-    def make_move(self, boardNum, Col):
+    def make_move(self, column):
         # Thinh's "make_move" function goes here
-        pass
+        # Take in 2 boards, and the column player 1 wants to place the disc,
+        # and return 2 new boards, and a flag (1 for win move, -1 for invalid move, 0 otherwise)
+        if 1 <= column <= 7:
+            if self.level_check(self.gameBoards[0], self.gameBoards[1], column) == 6:
+                return self.gameBoards[0], self.gameBoards[1], -1
+            if self.level_check(self.gameBoards[0], self.gameBoards[1], column) < 6:
+                self.board_change(self.gameBoards[0], (self.level_check(self.gameBoards[0], self.gameBoards[1], column) + 1, column))
+            if self.is_win(self.gameBoards[0]):
+                return self.gameBoards[0], self.gameBoards[1], 1
+            else:
+                return self.gameBoards[0], self.gameBoards[1], 0
+        else:
+            return self.gameBoards[0], self.gameBoards[1], -1
+
+    def board_change(self,board, position):
+        # Place a dic to a position with given board input and tuple for position
+        if 1 <= position[0] <= 6 and 1 <= position[1] <= 7:
+            board[7 * (position[0] - 1) + position[1] - 1] = 1
+
+    def board_value(self, board, position):
+        # Take in a board(list) and a tuple (k,s) where k is the level and s is the column,
+        # and return the value of the board at that position (-1 if given invalid position)
+        if 1 <= position[0] <= 6 and 1 <= position[1] <= 7:
+            return board[7 * (position[0] - 1) + position[1] - 1]
+        else:
+            return -1
+
+    def level_check(self, board1, board2, column):
+        # Take in 2 boards and a column, return the current level of the column (-1 if given invalid column)
+        if 1 <= column <= 7:
+            b1 = [self.board_value(board1, (k, column)) for k in range(6, 0, -1)]
+            b2 = [self.board_value(board2, (k, column)) for k in range(6, 0, -1)]
+            try:
+                pos1 = 6 - b1.index(1)
+            except ValueError:
+                pos1 = 0
+            try:
+                pos2 = 6 - b2.index(1)
+            except ValueError:
+                pos2 = 0
+            if pos1 == 6 or pos2 == 6:
+                return 6
+            if pos1 >= pos2:
+                return pos1
+            else:
+                return pos2
+        else:
+            return -1
+
+    def has_4_in_a_line(self, line):
+        # Check if there are four consecutive 1's on a line (list)
+        for i in range(len(line) - 3):
+            if line[i] & line[i + 1] & line[i + 2] & line[i + 3] == 1:
+                return True
+        return False
+
+    def is_win(self, board):
+        # Check if it is a winning board
+
+        # Check each level
+        for k in range(1, 7):
+            if self.has_4_in_a_line([self.board_value(board, (k, s)) for s in range(1, 8)]):
+                return True
+        # Check each column
+        for s in range(1, 8):
+            if self.has_4_in_a_line([self.board_value(board, (k, s)) for k in range(1, 7)]):
+                return True
+        # Check diagonals
+        for i in range(1, 4):
+            if self.has_4_in_a_line([self.board_value(board, (i + j, 1 + j)) for j in range(7 - i)]):
+                return True
+            if self.has_4_in_a_line([self.board_value(board, (1 + j, 1 + i + j)) for j in range(7 - i)]):
+                return True
+            if self.has_4_in_a_line([self.board_value(board, (i + j, 7 - j)) for j in range(7 - i)]):
+                return True
+            if self.has_4_in_a_line([self.board_value(board, (1 + j, 7 - i - j)) for j in range(7 - i)]):
+                return True
+        return False
 
 
 # the following lines are examples of how to use a GameBoard object
 g = GameBoard(-1)
 print(g)  # returns a graphical version of the current GameBoard object
 print(g.getBoards()[0])  # returns player1's board
-# g.make_move((PlayerNum-1), col) This will the the way that both a our neural network, and a player make moves, make_move already has access to both boards through the self indicator here
+g.make_move(6)
+print(g)
+# This will the the way that both a our neural network, and a player make moves, make_move already has access to both boards through the self indicator here
