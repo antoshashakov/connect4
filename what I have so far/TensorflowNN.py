2168a80515c0459536324c058741157be3c646c0
antoshashakov <83970932+antoshashakov@users.noreply.github.com>
2021-05-21 12:53:50 -0400

    Update TensorflowNN.py

--- what I have so far/TensorflowNN.py
+++ what I have so far/TensorflowNN.py
@@ -1,28 +1,31 @@
 import tensorflow as tf
 import importlib
+import math
+import numpy as np
 
 
 g_board = importlib.import_module('GameBoard') #Karl, Thinh
-# d_prob = importlib.import_module('desired_probabilities') #Ian, Uvernes
-
-#rough
 
 negloglik = lambda y, p_y: -p_y.log_prob(y) #log_likelihood custom
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Dense(1000, input_dim=126, activation='sigmoid'))
-model.add(tf.keras.layers.Dense(7, input_dim=1000, activation='softmax'))
+model.add(tf.keras.layers.Dense(7, activation='softmax'))
 
-model.compile(loss='negloglik', metrics='accuracy')
+model.compile(loss=negloglik, metrics='accuracy')
 
 g = g_board.GameBoard(-1)
-training_data = tf.constant(g.getBoards()[0] + g.getBoards()[1] + g.getBoards()[2])
-
+training_data = tf.constant([g.getBoards()[0] + g.getBoards()[1] + g.getBoards()[2]])
 print(training_data)
 
-target_data = g.desired_probabilities(model)
+
+target_data = tf.constant([g.desired_probabilities(model)])
+print(target_data)
 
 history = model.fit(training_data, target_data, epochs=10, verbose=2)
 
+
 print(model.predict(training_data))
+
+
 
