59abd1d80985522c1452f1e089ed2f140a3fa586
antoshashakov <83970932+antoshashakov@users.noreply.github.com>
2021-05-31 09:57:55 -0400

    Update TensorflowNN.py

--- what I have so far/TensorflowNN.py
+++ what I have so far/TensorflowNN.py
@@ -9,11 +9,12 @@ model.add(tf.keras.layers.Dense(7, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
 
+set_size = 2
 
 def init_data():
     training_data = []
     target_data = []
-    for i in range(0, 5):  # number of boards to be generated in our database
+    for i in range(0, set_size):  # number of boards to be generated in our database
         g = g_board.GameBoard(-1)
         print(g)
         train = [g.getBoards()[0] + g.getBoards()[1] + g.getBoards()[2]]
@@ -22,15 +23,16 @@ def init_data():
         print("desired_probabilities")
         print(des)
         target_data = target_data + [des]
-        print("predicted probs")
+        print("predicted_probabilities")
         print(model.predict(train))
     return tf.constant(training_data), tf.constant(target_data)
 
 
-for i in range(0, 3):
-    history = model.fit(init_data()[0], init_data()[1], verbose=2)
-    print(model.predict(init_data[0]))
-
+for i in range(0, 1):
+    train = init_data()[0]
+    history = model.fit(train, init_data()[1], verbose=2, batch_size=set_size, epochs=100)
+    print(train)
+    print(model.predict(train))
 
 
 
