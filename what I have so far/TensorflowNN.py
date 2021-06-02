a431d928f958b4e2ada1c728c330f354f02931f8
antoshashakov <83970932+antoshashakov@users.noreply.github.com>
2021-06-01 23:13:30 -0400

    Fixed board doubling by restructuring our code a little bit

--- what I have so far/TensorflowNN.py
+++ what I have so far/TensorflowNN.py
@@ -11,28 +11,34 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accura
 
 set_size = 2
 
-def init_data():
-    training_data = []
-    target_data = []
-    for i in range(0, set_size):  # number of boards to be generated in our database
-        g = g_board.GameBoard(-1)
-        print(g)
-        train = [g.getBoards()[0] + g.getBoards()[1] + g.getBoards()[2]]
-        training_data = training_data + train
-        des = g.desired_probabilities(model)
-        print("desired_probabilities")
-        print(des)
-        target_data = target_data + [des]
-        print("predicted_probabilities")
-        print(model.predict(train))
-    return tf.constant(training_data), tf.constant(target_data)
 
+def get_training_data(g):
+    training_data = []
+    print(g)
+    train = [g.getBoards()[0] + g.getBoards()[1] + g.getBoards()[2]]
+    training_data = training_data + train
+    print("predicted_probabilities")
+    print(model.predict(train))
+    return tf.constant(training_data)
+
+
+def get_target_data(g):
+    target_data = []
+    des = g.desired_probabilities(model)
+    print("desired_probabilities")
+    print(des)
+    target_data = target_data + [des]
+    return tf.constant(target_data)
+
+
+for i in range(0, set_size):  # number of boards to be generated in our database
+    g = g_board.GameBoard(-1)
+    trainer = get_training_data(g)
+    target = get_target_data(g)
 
 for i in range(0, 1):
-    train = init_data()[0]
-    history = model.fit(train, init_data()[1], verbose=2, batch_size=set_size, epochs=100)
-    print(train)
-    print(model.predict(train))
-
+    history = model.fit(trainer, target, verbose=2, batch_size=set_size, epochs=100)
+    print(trainer)
+    print(model.predict(trainer))
 
 
