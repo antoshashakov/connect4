803e26836db3fecd2f5213522d2ef04c0b2eed60
antoshashakov <83970932+antoshashakov@users.noreply.github.com>
2021-06-07 10:56:40 -0400

    Update TensorflowNN.py

--- what I have so far/TensorflowNN.py
+++ what I have so far/TensorflowNN.py
@@ -6,35 +6,17 @@ begin_time = datetime.datetime.now()
 print(datetime.datetime.now())
 g_board = importlib.import_module('GameBoard')  # Karl, Thinh
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(126, input_dim=126, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(126, input_dim=126, activation='relu'))
 model.add(tf.keras.layers.Dense(7, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
 
-set_size = 10
+set_size = 3
 
-def init_data():
-    training_data = []
-    target_data = []
-    for i in range(0, set_size):  # number of boards to be generated in our database
-        g = g_board.GameBoard(-1)
-        print(g)
-        train = [g.getBoards()[0] + g.getBoards()[1] + g.getBoards()[2]]
-        training_data = training_data + train
-        des = g.desired_probabilities(model)
-        print("desired_probabilities")
-        print(des)
-        target_data = target_data + [des]
-        print("predicted_probabilities")
-        print(model.predict(train))
-    return tf.constant(training_data), tf.constant(target_data)
-
-
-for i in range(0, 1):
-    train = init_data()[0]
-    history = model.fit(train, init_data()[1], verbose=2, batch_size=set_size, epochs=100)
-    print(train)
-    print(model.predict(train))
+for i in range(0, 2):
+    training = g_board.get_training_data(set_size, model)
+    target = g_board.get_target_data(g_board.tr_array, model)
+    history = model.fit(training, target, verbose=2, batch_size=set_size, epochs=100)
 print(datetime.datetime.now() - begin_time)
 
 
