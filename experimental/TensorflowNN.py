610d44670c2fd13069275fa0b8e7d9eb8702a4f2
ipriddle <84038467+ipriddle@users.noreply.github.com>
2021-06-21 11:58:54 -0400

    Update TensorflowNN.py
    Updated syntax to match with recent changes to GameBoard. Added evaluation set for testing (more details on evaluation function found in GameBoard class).

--- experimental/TensorflowNN.py
+++ experimental/TensorflowNN.py
@@ -11,10 +11,16 @@ model.add(tf.keras.layers.Dense(7, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
 
-set_size = 3
+set_size = 10
 
-for i in range(0, 2):
-    training = g_board.get_training_data(set_size, model)
-    target = g_board.get_target_data(g_board.tr_array, model)
-    history = model.fit(training, target, verbose=2, batch_size=set_size, epochs=100)
+eval_set = g_board.get_samples(10)
+g_board.evaluate(eval_set, model, True)
+
+
+for i in range(10):
+    boards = g_board.get_samples(set_size)
+    training = g_board.get_training_data(boards)
+    target = g_board.get_target_data(boards, model)
+    history = model.fit(training, target, verbose=0, batch_size=set_size, epochs=100)
+    g_board.evaluate(eval_set, model, True)
 print(datetime.datetime.now() - begin_time)
