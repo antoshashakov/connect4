d8790c3c15f68c75c47bfb1c301c24e966b3c3cd
antoshashakov <83970932+antoshashakov@users.noreply.github.com>
2021-06-22 23:24:49 -0400

    Update TensorflowNN.py

--- experimental/TensorflowNN.py
+++ experimental/TensorflowNN.py
@@ -6,21 +6,30 @@ begin_time = datetime.datetime.now()
 print(datetime.datetime.now())
 g_board = importlib.import_module('GameBoard')  # Karl, Thinh
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(126, input_dim=126, activation='relu'))
+model.add(tf.keras.layers.Dense(1000, input_dim=126, activation='sigmoid'))
 model.add(tf.keras.layers.Dense(7, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
+model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-set_size = 10
+testing = True
+
+set_size = 1000
+training_cycles = 10
+b_size = 100
+
+if testing:
+    set_size = 3
+    training_cycles = 5
+    b_size = 3
 
 eval_set = g_board.get_samples(10)
-g_board.evaluate(eval_set, model, True)
+g_board.evaluate(eval_set, model, True) #pre-training evaluation
 
 
-for i in range(10):
+for i in range(training_cycles):
     boards = g_board.get_samples(set_size)
     training = g_board.get_training_data(boards)
     target = g_board.get_target_data(boards, model)
-    history = model.fit(training, target, verbose=0, batch_size=set_size, epochs=100)
+    history = model.fit(training, target, verbose=2, batch_size=b_size, epochs=100)
     g_board.evaluate(eval_set, model, True)
 print(datetime.datetime.now() - begin_time)
